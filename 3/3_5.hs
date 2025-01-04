b6e66a9cd5302d05a77058764eafd2d858bd5a1f
Andrea Nerla <andreanerla96@gmail.com>
2025-01-04 17:57:07 +0100

    partial application example

--- 3/3_5.hs
+++ 3/3_5.hs
@@ -1,10 +1,18 @@
 --Use makeHtml in myhtml instead of using html_ and body_ directly
 
-body_ content = "<body>" <> content <> "</body>"
+body_ :: String -> String 
+body content = "<body>" <> content <> "</body>"
+
+html_ :: String -> String 
 html_ content = "<html>" <> content <> "</html>"
+
+head_ :: String -> String 
 head_ content = "<head>" <> content <> "</head>"
+
+title_ :: String -> String 
 title_ content = "<title>" <> content <> "</title>"
 
+makeHtml :: String -> String -> String
 makeHtml title body = html_(head_(title_(title)) <> body_(body))
 
 myHtml = makeHtml "Hello Title" "Hello World"
---
+++ 3/partial_application.hs
@@ -0,0 +1,11 @@
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+main = putStrLn (html_ "prova")
