f4e5d129049c41fe43fc95025f2382860f43ee8f
Andrea Nerla <andreanerla96@gmail.com>
2025-01-04 13:40:42 +0100

    hello.hs

--- 3/hello.hs
+++ 3/hello.hs
@@ -1,5 +1,10 @@
-main = putStrLn myhtml 
+body_ content = "<body>" <> content <> "</body>"
+html_ content = "<html>" <> content <> "</html>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
 
-wrapHtml content = "<html><body>" <> content <> "</body></html>"
+makeHtml title body = html_(head_(title_(title)) <> body_(body))
 
-myhtml =wrapHtml "Hello, world!"
+myHtml = makeHtml "Hello Title" "Hello World"
+
+main = putStrLn myHtml
