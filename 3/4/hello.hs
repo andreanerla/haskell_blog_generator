da70c149da8f94aeccc267a9e77161b4865f0830
Andrea Nerla <andreanerla96@gmail.com>
2025-01-18 20:30:49 +0100

    still working on hello 3/4

--- 3/4/hello.hs
+++ 3/4/hello.hs
@@ -1,14 +1,11 @@
--- hello.h
+-- hello.hs
 --Try changing the code we wrote in previous chapters to use the new types we created.
+
 
 --We can combine makeHtml and html_, and remove body_ head_ and title_ by calling el directly in html_, which can now have the type Title -> Structure -> Html. This will make our HTML EDSL less flexible but more compact.
 
 
 --Alternatively, we could create newtypes for HtmlHead and HtmlBody and pass those to html_, and we might do that in later chapters, but I've chosen to keep the API a bit simple for now, we can always refactor later!
-
-
-main :: IO ()
-main = putStrLn getComponentHtmlString myhtml
 
 
 newtype CompleteHtml = CompleteHtml String
@@ -17,41 +14,29 @@ newtype ComponentHtml = ComponentHtml String
 getComponentHtmlString :: ComponentHtml -> String
 getComponentHtmlString (ComponentHtml str) = str
 
+getCompleteHtmlString :: CompleteHtml -> String
+getCompleteHtmlString (CompleteHtml str) = str
+
 append_ :: ComponentHtml -> ComponentHtml -> ComponentHtml
 append_ (ComponentHtml a) (ComponentHtml b) = ComponentHtml (a <> b)
+
+el :: ComponentHtml -> ComponentHtml -> ComponentHtml
+el tag content =
+  ComponentHtml ("<" <> tag <> ">" <> content <> "</" <> tag <> ">")
+
+makeHtml :: ComponentHtml -> ComponentHtml -> CompleteHtml
+makeHtml title body = CompleteHtml(append_ (el "title" title) (el "body" body))
 
 myhtml :: CompleteHtml
 myhtml =
   makeHtml
-    "Hello title"
+    (el "Hello, title!") 
     (
-    h1_ "Hello, world!" <> 
-    p_ "Let's learn about Haskell!" <>
-    head "the head" <>
-    body "something about haskell" <>
+--    append ((el "h2" "Hello, world!"))
+--    el "p" "Let's learn about Haskell!" 
+--    el "head" "the head" 
+    el "something about haskell" 
     )
 
-makeHtml :: ComponentHtml -> ComponentHtml -> CompleteHtml
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
-
-html_ :: ComponentHtml -> CompleteHtml
-html_ = el "html"
-
-body_ :: String -> ComponentHtml
-body_ = el "body"
-
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
-el :: String -> String -> ComponentHtml
-el tag content =
-  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+main :: IO ()
+main = putStrLn (getCompleteHtmlString myhtml)
